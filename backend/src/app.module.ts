3925066f8a6b2570ceab91e10402a157d1c971b9
Franko Moraga <franko.moraga@estudiantes.uv.cl>
2026-06-18 13:06:34 -0400

    creación parcial de controladores

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb://admin:admin123@localhost:27017/footballdb?authSource=admin')],
